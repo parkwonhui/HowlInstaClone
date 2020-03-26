32084eb5eda174ca4d4433c2b7969e8943aaf220
parkwonhui <parkwonhui.dev@gmail.com>
2020-03-26 21:32:30 +0900

    add: upload photo and save firebase storage

--- src/main/java/com/test/howl_instaclone/MainActivity.kt
+++ src/main/java/com/test/howl_instaclone/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.test.howl_instaclone
 
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
+import com.test.howl_instaclone.navigation.AddPhotoActivity
 import com.test.howl_instaclone.navigation.AlarmFragment
 import com.test.howl_instaclone.navigation.DetailViewFragment
 import com.test.howl_instaclone.navigation.GridFragment
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
 
@@ -16,6 +22,9 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         bottom_navigation.setOnNavigationItemSelectedListener(this)
+
+        // authority
+        ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),1            )
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
@@ -31,6 +40,12 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
                 return true
             }
             R.id.action_add_photo -> {
+                // check use authority
+                if (ContextCompat.checkSelfPermission(
+                        this, android.Manifest.permission.READ_EXTERNAL_STORAGE )
+                == PackageManager.PERMISSION_GRANTED) {
+                    startActivity(Intent(this, AddPhotoActivity::class.java))
+                }
                 return true
             }
             R.id.action_favorite_alarm -> {
