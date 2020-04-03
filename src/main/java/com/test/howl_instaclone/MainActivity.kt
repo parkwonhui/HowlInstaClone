f6dd4f9b8b7eb0e3e4687d5ed42029417e337414
parkwonhui <parkwonhui.dev@gmail.com>
2020-04-04 00:10:48 +0900

    add: user page

--- src/main/java/com/test/howl_instaclone/MainActivity.kt
+++ src/main/java/com/test/howl_instaclone/MainActivity.kt
@@ -9,10 +9,8 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
-import com.test.howl_instaclone.navigation.AddPhotoActivity
-import com.test.howl_instaclone.navigation.AlarmFragment
-import com.test.howl_instaclone.navigation.DetailViewFragment
-import com.test.howl_instaclone.navigation.GridFragment
+import com.google.firebase.auth.FirebaseAuth
+import com.test.howl_instaclone.navigation.*
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.jar.Manifest
 
@@ -57,7 +55,11 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
                 return true
             }
             R.id.action_account -> {
-                var userFragment = DetailViewFragment()
+                var userFragment = UserFragment()
+                var bundle = Bundle()
+                var uid = FirebaseAuth.getInstance().currentUser?.uid
+                bundle.putString("destinationUid", uid)
+                userFragment.arguments = bundle
                 supportFragmentManager.beginTransaction().replace(R.id.main_content, userFragment).commit()
                 return true
             }
